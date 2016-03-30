64aa1f7eda1bc57c6d76a9a1ee9269b2fbd3e328
Mateo Acebedo <ma698@cornell.edu>
2016-03-29 21:50:23 -0700

    react added

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,22 @@ var webpack = require('webpack');
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js",
+  entry: "./js/client.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
   output: {
     path: __dirname + "/js",
-    filename: "scripts.min.js"
+    filename: "client.min.js"
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
