af8bed3ea7eb3845dbdb5082badfb04d97fc7b29
Mateo Acebedo <ma698@cornell.edu>
2016-04-06 00:32:21 -0700

    basic sankey displaying. Style is completely off

---
+++ src/js/todo.js
@@ -0,0 +1,40 @@
+//todo.js
+
+/*
+
+///before anything... come up with your bcklog and retro to the current month. Maybe the design that we are going for is not the right one. 
+
+1) make a container rectangle of size x
+2) calculated the number 
+
+the rectangle size depend on number of items and 
+2) make animation when opening a rectangle
+3) improve labeling 
+4) change color to some shade of blue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
