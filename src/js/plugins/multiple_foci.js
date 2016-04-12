99cc95daf65f6dc834be833ff3ed1b6205765eb1
Mateo Acebedo <ma698@cornell.edu>
2016-04-11 23:40:43 -0700

    improvements on the forcelayout

--- src/js/plugins/multiple_foci.js
+++ src/js/plugins/multiple_foci.js
@@ -4,23 +4,64 @@ d3.run_multiple_foci = function () {
   var width = 960,
       height = 500;
 
+  var margin = {top: 60, right: 60, bottom: 60, left: 60}
+
   var fill = d3.scale.category10();
 
-  var nodes = [],
-      foci = [{x: 150, y: 150}, {x: 350, y: 250}, {x: 700, y: 400}];
+  var x = d3.time.scale()
+        .domain([new Date("2016-01-01"), Date.now()])
+        .range([margin.left, width - margin.right]);
+
+  var foci = [{x: x(new Date("2016-01-01")), y: 250}, 
+              {x: x(new Date("2016-02-01")), y: 250}, 
+              {x: x(new Date("2016-04-01")), y: 250}];
 
   var svg = d3.select("#chart").append("svg")
       .attr("width", width)
       .attr("height", height);
 
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+    .ticks(d3.time.months, 1)
+    .tickFormat(d3.time.format('%m'))
+    .tickSize(0)
+    .tickPadding(8);
+
+  svg.append('g')
+    .attr('class', 'x axis')
+    .call(xAxis);
+
+  var nodes = d3.range(100).map(function(d, i) {
+    return {
+      x: Math.random() * width / 2,
+      y: Math.random() * height / 2,
+      r: Math.random() * 10 + 3,
+      id: ~~(Math.random() * 3),
+      r: ~~(Math.random() * 10)
+    };
+  });
+
   var force = d3.layout.force()
       .nodes(nodes)
       .links([])
-      .gravity(0)
+      .gravity(-.05)
+      .friction(.2)
+      .charge(function(d) { return -Math.pow(d.r, 4) / 8; })
       .size([width, height])
       .on("tick", tick);
 
-  var node = svg.selectAll("circle");
+  force.start();
+
+  var node = svg.selectAll("circle")
+        .data(nodes).enter().append("circle")
+        .attr("class", "node")
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", function(d) { return d.r * 5})
+        .style("fill", function(d) { return fill(d.id); })
+        .style("stroke", function(d) { return d3.rgb(fill(d.id)).darker(2); })
+        .call(force.drag);
 
   function tick(e) {
     var k = .1 * e.alpha;
@@ -35,20 +76,4 @@ d3.run_multiple_foci = function () {
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
   }
-
-  setInterval(function(){
-    nodes.push({id: ~~(Math.random() * foci.length)});
-    force.start();
-
-    node = node.data(nodes);
-
-    node.enter().append("circle")
-        .attr("class", "node")
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; })
-        .attr("r", 8)
-        .style("fill", function(d) { return fill(d.id); })
-        .style("stroke", function(d) { return d3.rgb(fill(d.id)).darker(2); })
-        .call(force.drag);
-  }, 500);
 }
