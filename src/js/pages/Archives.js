81270409bc67dc336922684b09e43a67c16a0c49
Mateo Acebedo <ma698@cornell.edu>
2016-04-11 23:47:35 -0700

    collision detection added

--- src/js/pages/Archives.js
+++ src/js/pages/Archives.js
@@ -1,10 +1,11 @@
 import React from "react";
 import foci from "../plugins/multiple_foci.js";
-
+import collisions from "../plugins/collision_detection.js";
 
 export default class Archives extends React.Component {
 	componentDidMount(){
-		d3.run_multiple_foci();
+		//d3.run_multiple_foci();
+		d3.run_collision_detection();
 	}
 
 	render() {
